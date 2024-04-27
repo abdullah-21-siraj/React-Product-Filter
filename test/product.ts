3f06230a14a39146dac51460c6c74ce6b3498e94
Abdullah Siraj <165858454+abdullah-21-siraj@users.noreply.github.com>
2024-04-27 15:03:36 +0500

    Add files via upload

---
+++ test/product.ts
@@ -0,0 +1,90 @@
+// products.js
+const products = [
+  {
+    id: 1,
+    image: "/randomPhoto.jpg",
+    price: "$99.99",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at condimentum lorem.",
+    category: "fruit",
+    availability: true,
+    show: "available",
+  },
+  {
+    id: 2,
+    image: "/randomPhoto.jpg",
+    price: "$49.99",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at condimentum lorem.",
+    category: "fruit",
+    availability: true,
+    show: "available",
+  },
+  {
+    id: 3,
+    image: "/randomPhoto.jpg",
+    price: "$29.99",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at condimentum lorem.",
+    category: "fruit",
+    availability: true,
+    show: "available",
+  },
+  {
+    id: 4,
+    image: "/randomPhoto.jpg",
+    price: "$29.99",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at condimentum lorem.",
+    category: "fruit",
+    availability: false,
+  },
+  {
+    id: 5,
+    image: "/randomPhoto.jpg",
+    price: "$29.99",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at condimentum lorem.",
+    category: "fruit",
+    availability: false,
+  },
+  {
+    id: 6,
+    image: "/randomPhoto.jpg",
+    price: "$29.99",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at condimentum lorem.",
+    category: "vegetable",
+    availability: false,
+  },
+  {
+    id: 7,
+    image: "/randomPhoto.jpg",
+    price: "$29.99",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at condimentum lorem.",
+    category: "vegetable",
+    availability: false,
+  },
+  {
+    id: 8,
+    image: "/randomPhoto.jpg",
+    price: "$29.99",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at condimentum lorem.",
+    category: "vegetable",
+    availability: false,
+  },
+  {
+    id: 9,
+    image: "/randomPhoto.jpg",
+    price: "$29.99",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at condimentum lorem.",
+    category: "vegetable",
+    availability: true,
+    show: "available",
+  },
+];
+
+export default products;
